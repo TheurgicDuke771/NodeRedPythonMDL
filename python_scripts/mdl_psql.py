d1d005bbaf9f36f0bf22af731fe5ce68c4bf239a
Arijit Roy <royarijit1995@gmail.com>
2023-03-18 13:24:37 +0530

    MDL: Reduce Num of scrapper API call for synopsis fetch

--- python_scripts/mdl_psql.py
+++ python_scripts/mdl_psql.py
@@ -2,13 +2,26 @@ import os
 import json
 import requests
 import psycopg2
+import pandas as pd
 from datetime import datetime
 from dotenv import load_dotenv
+import warnings
 
 
+warnings.filterwarnings('ignore')
 load_dotenv()
 
+
+def get_synopsis(item_url:str) -> str:
+	details_url = f"https://kuryana.vercel.app/id{item_url}"
+	response = requests.request("GET", details_url).json()
+	synopsis_str = str(response['data']['synopsis'])
+	synopsis = synopsis_str.replace("'", "''") if synopsis_str != '' else 'No synopsis available'
+	return synopsis
+
+
 def inset_into_db(content_list:list) -> int:
+	affected_rec_cnt = 0
 	now = datetime.now()
 	try:
 		conn = psycopg2.connect(
@@ -51,13 +64,6 @@ def inset_into_db(content_list:list) -> int:
 			except:
 				raise Exception("Type Not Found")
 			try:
-				details_url = f"https://kuryana.vercel.app/id{item['url']}"
-				response = requests.request("GET", details_url).json()
-				synopsis_str = str(response['data']['synopsis'])
-				synopsis = synopsis_str.replace("'", "''") if synopsis_str != '' else 'No synopsis available'
-			except:
-				synopsis = 'No synopsis available'
-			try:
 				datetime.strptime(item['released_at'], "%Y-%m-%d")
 				released_at = item['released_at']
 			except:
@@ -73,49 +79,83 @@ def inset_into_db(content_list:list) -> int:
 
 			table_name = 'movie' if c_type == 'Movie' else 'tv'
 
-			insert_query = f'''
-			INSERT INTO {table_name} 
-			("id", "title", "episodes", "ranking", "popularity",
-			"country", "content_type", "type", "synopsis",
-			"released_at", "url", "genres", "thumbnail",
-			"cover", "rating", "insert_ts", "update_ts") 
-			VALUES 
-			({id}, '{title}', {episodes}, {ranking}, {popularity},
-			'{country}', '{content_type}', '{c_type}', '{synopsis}',
-			'{released_at}', '{url}', '{genres}', '{thumbnail}',
-			'{cover}', {rating}, '{now.strftime("%Y-%m-%d %H:%M:%S")}',
-			'{now.strftime("%Y-%m-%d %H:%M:%S")}');
-			'''
-			update_query = f'''
-			UPDATE {table_name} SET 
-			"title" = '{title}',
-			"episodes" = {episodes}, 
-			"ranking" = {ranking}, 
-			"popularity" = {popularity}, 
-			"country" = '{country}',
-			"content_type" = '{content_type}', 
-			"type" = '{c_type}',
-			"synopsis" = '{synopsis}', 
-			"released_at" = '{released_at}', 
-			"url" = '{url}', 
-			"genres" = '{genres}', 
-			"thumbnail" = '{thumbnail}', 
-			"cover" = '{cover}', 
-			"rating" = {rating}, 
-			"update_ts" = '{now.strftime("%Y-%m-%d %H:%M:%S")}'
-			WHERE "id" = {id};
-			'''
 			fetch_query = f'SELECT * FROM {table_name} WHERE "id" = {id};'
-
-			cur.execute(fetch_query)
-			result = cur.fetchall()
+			result = pd.read_sql(fetch_query, conn)
+			
 			if len(result) == 0:
+				try:
+					synopsis = get_synopsis(item_url=item['url'])
+				except:
+					synopsis = 'No synopsis available'
+				insert_query = f'''
+				INSERT INTO {table_name} 
+				("id", "title", "episodes", "ranking", "popularity",
+				"country", "content_type", "type", "synopsis",
+				"released_at", "url", "genres", "thumbnail",
+				"cover", "rating", "insert_ts", "update_ts") 
+				VALUES 
+				({id}, '{title}', {episodes}, {ranking}, {popularity},
+				'{country}', '{content_type}', '{c_type}', '{synopsis}',
+				'{released_at}', '{url}', '{genres}', '{thumbnail}',
+				'{cover}', {rating}, '{now.strftime("%Y-%m-%d %H:%M:%S")}',
+				'{now.strftime("%Y-%m-%d %H:%M:%S")}');
+				'''
+				
 				cur.execute(insert_query)
 				conn.commit()
+				affected_rec_cnt += 1
 			else:
-				cur.execute(update_query)
-				conn.commit()
-		return len(content_list)
+				if result['synopsis'].values[0] == 'No synopsis available':
+					try:
+						synopsis = get_synopsis(item_url=item['url'])
+					except:
+						synopsis = 'No synopsis available'
+					update_query = f'''
+					UPDATE {table_name} SET 
+					"title" = '{title}',
+					"episodes" = {episodes}, 
+					"ranking" = {ranking}, 
+					"popularity" = {popularity}, 
+					"country" = '{country}',
+					"content_type" = '{content_type}', 
+					"type" = '{c_type}',
+					"synopsis" = '{synopsis}', 
+					"released_at" = '{released_at}', 
+					"url" = '{url}', 
+					"genres" = '{genres}', 
+					"thumbnail" = '{thumbnail}', 
+					"cover" = '{cover}', 
+					"rating" = {rating}, 
+					"update_ts" = '{now.strftime("%Y-%m-%d %H:%M:%S")}'
+					WHERE "id" = {id};
+					'''
+					cur.execute(update_query)
+					conn.commit()
+					affected_rec_cnt += 1
+				else:
+					update_query = f'''
+					UPDATE {table_name} SET 
+					"title" = '{title}',
+					"episodes" = {episodes}, 
+					"ranking" = {ranking}, 
+					"popularity" = {popularity}, 
+					"country" = '{country}',
+					"content_type" = '{content_type}', 
+					"type" = '{c_type}',
+					"released_at" = '{released_at}', 
+					"url" = '{url}', 
+					"genres" = '{genres}', 
+					"thumbnail" = '{thumbnail}', 
+					"cover" = '{cover}', 
+					"rating" = {rating}, 
+					"update_ts" = '{now.strftime("%Y-%m-%d %H:%M:%S")}'
+					WHERE "id" = {id};
+					'''
+					cur.execute(update_query)
+					conn.commit()
+					affected_rec_cnt += 1
+			
+		return affected_rec_cnt
 
 	except Exception as e:
 		print(e)
