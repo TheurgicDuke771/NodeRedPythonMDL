6d2e9244fa822101e4a2dafc0ee9d7c915b5d5f3
Arijit Roy <royarijit1995@gmail.com>
2023-07-16 18:12:35 +0530

    MDL: updated invalid record script

--- python_scripts/mdl_invalid_record_update.py
+++ python_scripts/mdl_invalid_record_update.py
@@ -15,9 +15,6 @@ for db_name in DB_LIST:
         fetch_query = f"select * from public.{db_name} where DATE_PART('day', localtimestamp(0) - update_ts) > 30;"
         df = sqlio.read_sql_query(fetch_query, conn)
         print(f"{ETL_TIME} INFO: Fetched {len(df)} records for DB {db_name}")
-        fetch_query = f"select * from public.{db_name} where DATE_PART('day', localtimestamp(0) - update_ts) > 30;"
-        df = sqlio.read_sql_query(fetch_query, conn)
-        print(f"{ETL_TIME} INFO: Fetched {len(df)} records")
 
         id_list = []
         for index, row in df.iterrows():
@@ -41,29 +38,6 @@ for db_name in DB_LIST:
             print(f"{ETL_TIME} INFO: {len(id_list)} records are updated for DB {db_name}")
         else:
             print(f"{ETL_TIME} INFO: No record to update for DB {db_name}")
-        id_list = []
-        for index, row in df.iterrows():
-            response = requests.request("GET", row["url"])
-            if response.status_code == 404:
-                id_list.append(str(row["id"]))
-        if len(id_list) != 0:
-            update_query = f"""
-            update 
-                public.{db_name} 
-            set 
-                ranking = 99999, 
-                released_at = '2099-12-31', 
-                rating = 0, 
-                update_ts = '{ETL_TIME}'
-            where 
-                id in ({','.join(id_list)});
-            """
-            cur.execute(update_query)
-            conn.commit()
-            print(f"{ETL_TIME} INFO: {len(id_list)} records are updated")
-        else:
-            print(f"{ETL_TIME} INFO: No record to update")
-
     except Exception as e:
         print(f"{ETL_TIME} ERROR: {str(e)}")
 
